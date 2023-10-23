c49d58d04e148243356e36d57e45b3348491bdd4
shahid <shahidhashmiraj@gmail.com>
2023-10-23 22:24:22 +0530

    lower bound

--- app.js
+++ app.js
@@ -18,5 +18,23 @@ console.log(search([-1,0,3,5,9,12],9))
 
 // ============== Lower Bound ============================
 
- 
+let array = [1,2,3,4,5,6,7,8,9]
+let n = array.length;
 
+function LowerBound(arr, target, n){
+    let lower = arr[0];
+    let higher = arr[arr.length - 1];
+    let ans = n;
+    
+    while(lower <= higher){
+        let middle = Math.floor((lower + higher) / 2)
+        if(arr[middle] >=target){
+            ans = middle;
+            higher = middle -1
+        }else{
+            low = middle+1
+        }
+    }
+    return ans
+}
+console.log(LowerBound(array,1,n))
